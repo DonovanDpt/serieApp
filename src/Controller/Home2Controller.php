8a5d433b4aa35e47f7365b53bd56dd95430e3c87
Stagiaire <dupratdonovan@gmail.com>
2023-04-25 08:37:40 +0200

    Test cours

---
+++ src/Controller/Home2Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Home2Controller extends AbstractController
+{
+    #[Route('/index', name: 'home_index')]
+    public function index(): Response
+    {
+        return $this->render('home2/index.html.twig', [
+            'controller_name' => 'Home2Controller',
+        ]);
+    }
+
+    #[Route('/index2', name: 'home2_index2')]
+
+    public function index2(): Response
+    {
+        $prenom = "Donovan";
+        return $this->render(
+            'home2/index.html.twig', [
+            "prenom" => $prenom,
+        ]
+        );
+    }
+}
